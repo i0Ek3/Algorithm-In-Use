b22ec312abac5d423fb39dd8d186f6fe53b693e5
i0Ek3 <kno30826@gmail.com>
2017-10-30 16:34:13 +0800

    merge codes into code/

---
+++ code/1_print.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b); //dont forget & exclude char type
+    printf("%d",a+b);
+    return 0;
+}
---
+++ code/2_double.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+    double a = 3.14,b = 0.15;//do not use float whatever
+    double c = a + b;
+    printf("%f",c);
+    return 0;
+}
