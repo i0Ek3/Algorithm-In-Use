8a833a5b3dd465c8c584d5ad52e0bbadcf728723
i0Ek3 <kno30826@gmail.com>
2017-11-18 09:45:42 +0800

    modified something then still boom

--- code/codeup_1928-v1.cpp
+++ code/codeup_1928-v1.cpp
@@ -4,26 +4,34 @@
 //则diff = sum1 + (365 - sum2),此为连续年份.
 //非连续年份diff = abs(y1 - y2)365 + sum1 + (365 - sum2)
 //此段程序有问题,心不在焉,下次修复吧!
+//11-18:将两个函数合并为一个,增加了形参,依然有点boom
 
 #include <iostream>
 #include <cstdio>
-#include <math.h>
+#include <cmath>
 
 //Golobal var
 int year1,year2,month1,month2,date1,date2,cur1,cur2,diff;
-int day1[] = {31,28,31,30,31,30,31,31,30,31,30,31};
-int day2[] = {31,29,31,30,31,30,31,31,30,31,30,31};
+int day2[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+int day1[] = {31,29,31,30,31,30,31,31,30,31,30,31};
 int sum1 = 0;
 int sum2 = 0;
-int leap = 365;//闰年天数
-int unleap = 366;//非闰年天数
+int leap = 365;//平年天数
+int unleap = 366;//闰年天数
 int cnt = 0;//闰年个数
 
-void calDate(int year1,int month1,int date1,int year2,int month2,int date2)
+void calDate(int year1,int month1,int date1,int year2,int month2,int date2,int cur1,int cur2)
 {
+    if(year1 > year2)
+    {
+        int temp = year1;
+        year1 = year2;
+        year2 = temp;
+    }
+
     for(int i = 0; i < month1; i++)//计算第一个输入的年份日期的当前天数
     {
-        if(year1 % 400 != 0 || (year1 % 4 == 0 && year1 % 100 == 0))
+        if(year1 % 400 == 0 || (year1 % 4 == 0 && year1 % 100 != 0))
         {
             sum1 += day1[i];
             cur1 = sum1 + date1;//当前闰年年份的天数
@@ -37,7 +45,7 @@ void calDate(int year1,int month1,int date1,int year2,int month2,int date2)
     
     for(int j = 0; j < month2; j++)//计算第二个输入的年份日期的当前天数
     {
-        if(year2 % 400 != 0 || (year2 % 4 == 0 && year2 % 100 == 0))
+        if(year2 % 400 == 0 || (year2 % 4 == 0 && year2 % 100 != 0))
         {
             sum2 += day1[j]; 
             cur2 = leap - (sum2 + date2);
@@ -48,12 +56,8 @@ void calDate(int year1,int month1,int date1,int year2,int month2,int date2)
             cur2 = unleap - (sum2 + date2);  
         }    
     }
-}
     
-//void diffDate()//计算相差天数
-void diffDate(int year1,int month1,int date1,int year2,int month2,int date2)
-{
-    if (year1 + 1 == year2) //|| year2 + 1 == year1)//连续年份相差天数
+    if (year1 + 1 == year2)//连续年份相差天数
     {
         diff = cur1 + cur2;
         printf("%d\n",diff);
@@ -62,25 +66,22 @@ void diffDate(int year1,int month1,int date1,int year2,int month2,int date2)
     {
         for(int i = year1; i <= year2; i++)
         {
-            if(i % 400 != 0 || (i % 4 == 0 && i % 100 == 0))
+            if(i % 400 == 0 || (i % 4 == 0 && i % 100 != 0))
             {
                 cnt++;
             }
+            diff = cur1 + cur2 + cnt*std::abs((year1-year2));
         }
-        diff = cur1 + cur2 + cnt*(year1-year2);
         printf("%d\n",diff);
     }
 }
 
 int main()
 {
-    while(scanf("%4d%2d%2d\n%4d%2d%2d\n",&year1,&month1,&date1,&year2,&month2,&date2) != EOF)
+    while(scanf("%d%d%d%d%d%d\n",&year1,&month1,&date1,&year2,&month2,&date2) != EOF)
     {
-        //calDate();
-        calDate(year1,month1,date1,year2,month2,date2);
+        calDate(year1,month1,date1,year2,month2,date2,cur1,cur2);
     }
-    //diffDate();
-    diffDate(year1,month1,date1,year2,month2,date2);
 
     return 0;
 }
