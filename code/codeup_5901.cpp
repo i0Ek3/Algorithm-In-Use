4d35718c0898df26602d626b9cfedd63b1ddb9d7
i0Ek3 <kno30826@gmail.com>
2017-11-19 18:23:02 +0800

    bug is waiting...

---
+++ code/codeup_5901.cpp
@@ -0,0 +1,38 @@
+//回文串
+//12321 level ava &8&
+//思路:将输入的字符存入数组中
+//判断数组中从前往后和从后往前对应的数是否相等
+//偶数不可能为回文数
+//
+#include <stdio.h>
+#include <cstring>
+
+int main()
+{
+    char str[255];
+    int i;
+    
+    while(scanf("%s",str))
+    {
+        int len = strlen(str);
+        if(len % 2 == 0)//even不可能为回文数 
+        {
+            printf("NO\n");
+        }
+        else//odd进一步判断
+        {
+            for(i = 0; i < len/2; i++)
+            {
+                if(str[i] != str[len-i-1])
+                {
+                    printf("NO\n");
+                }
+                else
+                {
+                    printf("YES\n");
+                }
+            }
+        }
+    }
+    return 0;
+}
