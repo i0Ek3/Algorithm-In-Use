54594ef93aa89cd7ccf2f6bdd4c4e758283ef9d2
i0Ek3 <kno30826@gmail.com>
2017-12-09 09:45:32 +0800

    update permutation.cpp

--- code/chp04/recursion/permutation.cpp
+++ code/chp04/recursion/permutation.cpp
@@ -7,17 +7,18 @@
 #include <cstdio>
 
 const int maxn = 11;
-int n,p[maxn],hashTable[maxn] = {false};
+int n,p[maxn];//stored current permutation
+int hashTable[maxn] = {false};
 
 void generateP(int index)
 {
-    if (index == n + 1)
+    if (index == n + 1)//recursion borden
     {
         for (int i = 1; i <= n; i++)
         {
             printf("%d",p[i]);
         }
-        printf("\n");
+        printf(" ");
         return;
     }
     for (int x = 1; x <= n; x++)
