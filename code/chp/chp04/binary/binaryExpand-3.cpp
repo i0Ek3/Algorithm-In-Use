7641f76885e0126fcdb58488ce24bb915073aa6a
i0Ek3 <kno30826@gmail.com>
2018-01-22 11:20:19 +0800

    uncompleted

---
+++ code/chp/chp04/binary/binaryExpand-3.cpp
@@ -0,0 +1,20 @@
+ // 
+ // binaryExpand-3.cpp
+ // ianpasm(kno30826@gmail.com)
+ // 2018-01-22 11:06:42
+ // 
+ 
+
+//cut turb 
+//Given specific length of N turbs,cut them to get K segments of same length,so how long of K segments?
+
+
+
+
+
+
+
+//find the biggest radius
+//Given specific length of N line segments then joint them whatever order and make it be a convex polygon and find the biggest radius of it's circumscribed
+//
+//
