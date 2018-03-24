c540b3905d4fd7dfec41dba88467615d5f3022c5
i0Ek3 <kno30826@gmail.com>
2018-03-24 11:09:29 +0800

    add algorithm.cpp

---
+++ code/chp/chp06/algorithm.cpp
@@ -0,0 +1,25 @@
+// 
+// algorithm.cpp
+// @ianpasm(kno30826@gmail.com)
+// 2018-03-24 11:01:51
+// 
+ 
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    //max();
+    //min();
+    //abs();
+    //swap();
+    //reverse();
+    //next_permutation(); //output a seq under its full arrange
+    //fill(); //assign
+    //sort(); // in container just these type can use sort():vector,string,deque
+              // like set and map,its based on red black tree,that means its already in order.      
+    //lower_bound();
+    //upper_bound();
+    return 0;
+}
