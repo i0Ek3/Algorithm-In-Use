fc98b2a650bcb0ad648da5c7e7b65525c1a0aeb0
i0Ek3 <kno30826@gmail.com>
2018-03-31 20:05:20 +0800

    add BFS note

---
+++ code/chp/chp08/BFS.cc
@@ -0,0 +1,26 @@
+// 
+// BFS.cc
+// @ianpasm(kno30826@gmail.com)
+// 2018-03-31 10:12:38
+//
+// @Note
+// BFS always implement by queue, and always traverse itself by level orderly.
+// There is a template to use, so get it now:
+//     
+//     void BFS(int s)
+//     {
+//          queue<int> q;
+//          q.push(s);
+//          while (!q.empty())
+//          {
+//              //take top ele out;
+//              //visit top ele;
+//              //pop top ele;
+//              //push all un-enqueue elements into queue on the next level of top and set it status with true.
+//          }
+//     }
+//
+//
+//
+ 
+
