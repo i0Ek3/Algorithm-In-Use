3ef96fe302d27ca5eec67beb3d270df187f6fe8e
i0Ek3 <kno30826@gmail.com>
2018-04-16 09:40:45 +0800

    later...

---
+++ code/chp/chp09/pat_a1043.cc
@@ -0,0 +1,21 @@
+// 
+// pat_a1043.cc
+// @ianpasm(kno30826@gmail.com)
+// 2018-04-16 09:32:35
+//
+// 
+// @Is it a BST?
+//  Given a sequence of integer keys, if the tree is the preorder traverse or the mirror image of a BST.Please output the tree's postorder traverse.
+//
+//
+//
+//
+//
+//
+//
+ 
+#include <cstdio>
+#include <vector>
+using std::vector;
+
+int const maxn = 110;
