d2a1c689ea8c5addfc380e3d757d93de8f7a0cec
i0Ek3 <kno30826@gmail.com>
2018-04-19 10:00:31 +0800

    finished remainder

--- code/chp/chp09/AVL.cc
+++ code/chp/chp09/AVL.cc
@@ -5,17 +5,19 @@
 // 
 //
 // @AVL Tree
+//  You need adjust the balance factor of tree repeatly until reached balance.
 //
-//
-//
-//
+
+
+
 #include <cstdio>
 #include <algorithm>
 using namespace std;
 
 struct node {
     int v, height, data;
-    node* rchild, lchild;
+    node* rchild;
+    node* lchild;
 };
 
 node* newNode(int v)
@@ -37,7 +39,7 @@ int getHeight(node* root)
     return root->height;
 }
 
-int getBlanceFactor(node* root)
+int getBalanceFactor(node* root)
 {
     return getHeight(root->lchild) - getHeight(root->rchild);
 }
@@ -53,11 +55,12 @@ void search(node* root, int x)
     if (root == NULL)
     {
         printf("search failed.\n");
-        return 0;
+        return;
     }
+
     if (x == root->data)
     {
-        printf("%d\n", root->x);
+        printf("%d\n", root->data);
     }
     else if (x < root->data)
     {
@@ -69,5 +72,88 @@ void search(node* root, int x)
     }
 }
 
+//left rotation
+void LR(node* &root)
+{
+    node* temp = root->rchild;
+    root->rchild = temp->lchild;
+    temp->lchild = root;
+    updateHeight(root);
+    updateHeight(temp);
+    root = temp;
+}
 
+void RR(node* &root)
+{
+    node* temp = root->lchild;
+    root->lchild = temp->rchild;
+    temp->rchild = root;
+    updateHeight(root);
+    updateHeight(temp);
+    root = temp;
+}
+
+//judge whether lost balance
+void insert(node* &root, int v)
+{
+    if (root == NULL)
+    {
+        root = newNode(v);
+        return;
+    }
+
+    if (v < root->v)
+    {
+        insert(root->lchild, v);
+        updateHeight(root);
+        if (getBalanceFactor(root) == 2)
+        {
+            if (getBalanceFactor(root->lchild) == 1)
+            {
+                RR(root);
+            }
+            else if (getBalanceFactor(root->lchild) == -1)
+            {
+                LR(root->lchild);
+                RR(root);
+            }
+        }
+    }
+    else
+    {
+        insert(root->rchild, v);
+        updateHeight(root);
+        if (getBalanceFactor(root) == -2)
+        {
+            if (getBalanceFactor(root->rchild) == -1)
+            {
+                LR(root);
+            }
+            else if (getBalanceFactor(root->rchild) == 1)
+            {
+                RR(root->rchild);
+                LR(root);
+            }
+        }
+    }
+}
+
+node* buildTree(int data[], int n)
+{
+    node* root = NULL;
+    for (int i = 0; i < n; i++)
+    {
+        insert(root, data[i]);
+    }
+    return root;
+}
+
+
+int main()
+{
+
+
+
+    return 0;
+}
 
