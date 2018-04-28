1c39da6265d21d14c8437e02fa8e25efc3541c09
i0Ek3 <kno30826@gmail.com>
2018-04-28 11:02:27 +0800

    add entirety frame

--- code/extra/tree/rbtree.cc
+++ code/extra/tree/rbtree.cc
@@ -4,10 +4,27 @@
 // 2018-04-22 16:09:35
 // 
 // @Reb Black Tree 
-//  Ref: https://bbs.pediy.com/thread-225432.html
+//  Ref1: https://bbs.pediy.com/thread-225432.html
+//  Ref2: http://www.sohu.com/a/201923614_466939
 //
+// @Nature
+//  BST's nature (but there is a flaw, it has line performance)
+//  Auto-banlance
+//  Nodes' color is red or black
+//  Root node's color is black
+//  All leaf nodes' color is black and it's value is NULL
+//  All red nodes' two subnodes' color is black, cannot exisit two continuous red nodes
+//  Any nodes come to it's leaf nodes of path which have same numbers black nodes
 //
+// @Fix Up
+//  Change the color
+//      Black -> Red
+//      Red -> Black
+//  Roation
+//      LR
+//      RR
 //
+// 
 //
 //
  
