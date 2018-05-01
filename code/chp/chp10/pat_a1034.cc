97b3772983a09124c7fd25d3ec6c74d0358858c0
i0Ek3 <kno30826@gmail.com>
2018-05-01 10:23:05 +0800

    add pat_a1076.cc & pat_a1034.cc

--- code/chp/chp10/pat_a1034.cc
+++ code/chp/chp10/pat_a1034.cc
@@ -7,5 +7,99 @@
 // @Head of a Gang
 //  Maybe later, I don't know what talking about of this question.Let me try later...
 //
+// 
  
+#include <iostream>
+#include <string>
+#include <map>
+#include <iterator>
+
+const int maxn = 2001;
+const int INF = 0x3fffffff;
+
+std::map<int, std::string> intToString; //id -> name 
+std::map<std::string, int> stringToInt; //name -> id 
+std::map<std::string, int> Gang; // head -> number of person 
+
+int G[maxn][maxn] = {0}; //adjacent matrix 
+int weight[maxn] = {0}; //weight of point 
+int n, k, numPerson = 0; 
+bool visit[maxn] = {false};
+
+void DFS(int nowVisit, int &head, int &numMember, int &totalValue)
+{
+    numMember++;
+    visit[nowVisit] = true;
+    if (weight[nowVisit] > weight[head])
+    {
+        head = nowVisit; //update head 
+    }
+    for (int i = 0; i < numPerson; i++)
+    {
+        if (G[nowVisit][i] > 0)
+        {
+            totalValue += G[nowVisit][i]; //add weight of line 
+            G[nowVisit][i] = G[i][nowVisit] = 0; //delete that line avoid to recount 
+            if (visit[i] == false)
+            {
+                DFS(i, head, numMember, totalValue);
+            }
+        }
+    }
+}
+
+void DFSTrave()
+{
+    for (int i = 0; i < numPerson; i++)
+    {
+        if (visit[i] == false)
+        {
+            int head = i, numMember = 0, totalValue = 0;
+            DFS(i, head, numMember, totalValue);
+            if (numMember > 2 && totalValue > k)
+            {
+                Gang[intToString[head]] = numMember;
+            }
+        }
+    }
+}
+
+int change(std::string str)
+{
+    if (stringToInt.find(str) != stringToInt.end())
+    {
+        return stringToInt[str];
+    }
+    else
+    {
+        stringToInt[str] = numPerson;
+        intToString[numPerson] = str;
+        return numPerson;
+    }
+}
+
+int main()
+{
+    int w;
+    std::string str1, str2;
+    std::cin >> n >> k;
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> str1 >> str2 >> w;
+        int id1 = change(str1);
+        int id2 = change(str2);
+        weight[id1] += w;
+        weight[id2] += w;
+        G[id1][id2] += w;
+        G[id2][id1] += w;
+    }
+    DFSTrave();
+    std::cout << Gang.size() << std::endl;
+    std::map<std::string, int>::iterator it;
+    for (it = Gang.begin(); it != Gang.end(); it++)
+    {
+        std::cout << it->first << " " << it->second << std::endl;
+    }
+    return 0;
+}
 
--- code/chp/chp10/pat_a1076.cc
+++ code/chp/chp10/pat_a1076.cc
@@ -5,7 +5,77 @@
 // 
 // @Forwards on Weibo
 //
-// Later...
 //
  
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <queue>
 
+const int MAXV = 1010;
+
+struct Node 
+{
+    int id;
+    int layer;
+};
+
+std::vector<Node> Adj[MAXV]; //Adjacent link
+bool inqueue[MAXV] = {false};
+
+int BFS(int s, int L)
+{
+    int numForward = 0;
+    std::queue<Node> q;
+    Node start;
+    start.id = s;
+    start.layer = 0;
+    q.push(start);
+    inqueue[start.id] = true;
+    while (!q.empty())
+    {
+        Node topNode = q.front();
+        q.pop();
+        int u = topNode.id; //top node's id 
+        for (int i = 0; i < Adj[u].size(); i++)
+        {
+            Node next = Adj[u][i]; //start at u can reached node next
+            next.layer = topNode.layer + 1;
+            if (inqueue[next.id] == false && next.layer <= L)
+            {
+                q.push(next);
+                inqueue[next.id] = true;
+                numForward++;
+            }
+        }
+    }
+    return numForward;
+}
+
+
+int main()
+{
+    Node user;
+    int n, L, numForward, idFollow;
+    scanf("%d%d", &n, &L);
+    for (int i = 1; i <= n; i++)
+    {
+        user.id = i;
+        scanf("%d", &numForward);
+        for (int j = 0; j < numForward; j++)
+        {
+            scanf("%d", &idFollow);
+            Adj[idFollow].push_back(user); //line idFollow -> i
+        }
+    }
+    int numQuery, s;
+    scanf("%d", &numQuery); //the number of query 
+    for (int i = 0; i < numQuery; i++)
+    {
+        memset(inqueue, false, sizeof(inqueue));
+        scanf("%d", &s);
+        int numForward = BFS(s, L);
+        printf("%d\n", numForward);
+    }
+    return 0;
+}
