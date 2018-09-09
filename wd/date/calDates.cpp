a6a517d0dc019473436d7012e28a1dd225ba6917
i0Ek3 <kno30826@gmail.com>
2018-09-09 16:08:08 +0800

    Update calDates.cpp

--- wd/date/calDates.cpp
+++ wd/date/calDates.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstring>
 #include <algorithm>
 
 // yeap confirmation
@@ -7,9 +8,19 @@
 // limitation
 int buf[10000][13][32];
 
-// days of month 
+// day of month 
 int dayOfMonth[13][2] = {
     0,0,31,31,28,29,31,31,30,30,31,31,30,30,31,31,31,31,30,30,31,31,30,30,31,31
+};
+
+// name of month
+char nameOfMonth[13][5] = {
+    "", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" 
+};
+
+// name of week
+char nameOfWeek[7][5] = {
+    "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
 };
 
 struct Date{
@@ -29,13 +40,25 @@ struct Date{
     }
 };
 
+void datesCalculate() {
+    
+    int d1, m1, y1, d2, m2 ,y2;
+    printf("Please input date(s) like this: 20180809 20110201: \n");
+    while (scanf("%4d%2d%2d", &y1, &m1, &d1) != EOF) {
+        scanf("%4d%2d%2d", &y2, &m2, &d2);
+        printf("The differ of your input is: %d\n", abs(buf[y1][m1][d1] - buf[y2][m2][d2]) + 1);
+    }
+}
+
 int main()
 {
+    //datesCalculate();
     Date tmp;
     int cnt = 0;
     tmp.Day = 1;
     tmp.Month = 1;
     tmp.Year = 0;
+    
     while (tmp.Year != 10000) {
         
         // save differs with date 0000-01-01
@@ -44,10 +67,22 @@ int main()
         cnt++;
     }
 
-    int d1, m1, y1, d2, m2 ,y2;
-    while (scanf("%4d%2d%2d", &y1, &m1, &d1) != EOF) {
-        scanf("%4d%2d%2d", &y2, &m2, &d2);
-        printf("%d\n", abs(buf[y1][m1][d1] - buf[y2][m2][d2]) + 1);
+    // weekName
+    int d, m, y;
+    char s[5];
+    printf("Please input date(s) like this: 2001 Oct 1: \n");
+    while (scanf("%d%s%d", &y, s, &d) != EOF) {
+        for (int m = 1; m < 13; m++) {
+            if (strcmp(s, nameOfMonth[m]) == 0) {
+                break;
+            }
+        }
+        int days = buf[y][m][d] - buf[2018][9][9]; 
+        days += 1;
+        printf("The date of your input is: ");
+        puts(nameOfWeek[(days % 7 + 7) % 7]);
+        //fixme
+        printf("And today is %dth day of this year!", abs(buf[y][m][d] - buf[y][1][1]) + 1);
     }
 
     return 0;
