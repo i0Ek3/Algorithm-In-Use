8d624062b08e476382274e2c3cee3039bbc6b03d
i0Ek3 <kno30826@gmail.com>
2018-09-20 17:18:45 +0800

    Update gcd.cc

--- wd/math/gcd.cc
+++ wd/math/gcd.cc
@@ -3,6 +3,10 @@
 int gcd(int a, int b) {
         if (b == 0) {
                 return a;
+        } else if (a == 0) {
+                return b;
+        } else if (a == 0 && b == 0) {
+                printf("No gcd yet!");
         } else {
                 return gcd(b, a % b);
         } 
